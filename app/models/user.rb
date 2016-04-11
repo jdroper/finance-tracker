672618cc48b606b744863b74044b0c4f9e67703f
Justin Roper <jdroper@neo.rr.com>
2016-04-10 23:07:30 -0400

    add user and stock associtiaon and display

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :user_stocks
+  has_many :stocks, through: :user_stocks
+
+# check if its over the 10 stock limit
+  def can_add_stock?(ticker_symbol)
+    under_stock_limit? && !stock_already_added?(ticker_symbol)
+  end
+
+  def under_stock_limit?
+    (user_stocks.count < 10)
+  end
+
+# Check to see if they already added to follow the stock
+  def stock_already_added?(ticker_symbol)
+    stock = Stock.find_by_ticker(ticker_symbol)
+    return false unless stock
+    user_stocks.where(stock_id: stock.id).exists?
+  end
+
 end
